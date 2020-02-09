aa16a4bae890cfbb40b5dd354e1c96760c23c0cf
Udai Raria <udairajput0@gmail.com>
2020-02-09 15:17:52 +0530

    fix service worker path

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(morgan('tiny'));
 app.use(cors());
 //app.use(history());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'docs')));
 //app.use('/', require('./routes/activity'))
 
 
@@ -235,7 +235,7 @@ app.get("/api/users/:email", function(req, res) {
 })
 
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, 'docs/index.html'));
 })
 
 app.set('PORT', process.env.PORT || 3000);
